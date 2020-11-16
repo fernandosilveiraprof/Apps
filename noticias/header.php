0604f3f32b8879e79d50e6b142daad0420c203c0
fernandosilveiraprof <fernandosilveiraprof@gmail.com>
2020-11-16 18:06:57 -0300

    ajustes do header

---
+++ noticias/header.php
@@ -0,0 +1,19 @@
+
+<header>
+	<figure>
+		<img src="" alt="Logo Aqui"> 
+	</figure>
+
+	<nav>
+		<ul>
+			<li><a href="" item2="">Início</a></li>
+			<li><a href="" item1="">Cadastrar Notícia </a></li>
+			
+
+			
+		</ul>
+	</nav>
+	<form action="search" >
+		<input type="text" placeholder="Digite o conteúdo que procura..." >
+	</form>
+</header>
