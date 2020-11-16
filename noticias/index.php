6d6c17037cab6d79263707b7b1c1242d4d20f5d0
fernandosilveiraprof <fernandosilveiraprof@gmail.com>
2020-11-16 18:25:06 -0300

    incluindo header na index

---
+++ noticias/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Systema Básico de notícias </title>
+	<link rel="stylesheet" href="http://meyerweb.com/eric/tools/css/reset/">
+	<link rel="stylesheet" href="_css/style.css">
+</head>
+<body>
+	<?php require("header.php") ?>
+	
+</body>
+</html>
